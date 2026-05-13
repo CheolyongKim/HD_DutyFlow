427a096318326897ce1c474d8fdc15effae81ceb
0-x-14 <0_x_14@naver.com>
2026-05-13 19:03:04 +0900

    Refactor: 장바구니 상품 추가 기능 예외처리 추가

--- src/cart/Cart.java
+++ src/cart/Cart.java
@@ -3,6 +3,9 @@ package cart;
 import java.util.HashMap;
 import java.util.Map;
 
+import exception.DataNotFoundException;
+import exception.ErrorCode;
+import exception.ValidationException;
 import product.Product;
 
 public class Cart {
@@ -11,6 +14,16 @@ public class Cart {
 	
 	// 장바구니에 상품 추가
     public void addToCart(Product p, int wishAmount) {
+    	
+        // 상품 정보가 없는 경우
+        if (p == null) {
+            throw new DataNotFoundException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+
+        // 추가하려는 수량이 0 이하인 경우
+        if (wishAmount <= 0) {
+            throw new ValidationException(ErrorCode.INVALID_QUANTITY);
+        }
 
     	// 이미 장바구니에 존재하는 상품이면 기존 수량에 추가
     	// 존재하지 않으면 새로 추가
