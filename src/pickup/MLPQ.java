e45a659b4002df15cd3d6840541242e8cffae493
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-13 11:16:56 +0900

    Add MLPQ mandatory fields

--- src/pickup/MLPQ.java
+++ src/pickup/MLPQ.java
@@ -1,5 +1,12 @@
 package pickup;
 
-public class MLPQ {
+import java.util.Map;
+import java.util.PriorityQueue;
 
+public class MLPQ {
+	private PriorityQueue<Map<Airplane, Member>> aq;
+	private PriorityQueue<Map<Airplane, Member>> aq;
+	
+	private int promotionThresholdMinutes;
+	private int maxWaitTimeMinutes;
 }
