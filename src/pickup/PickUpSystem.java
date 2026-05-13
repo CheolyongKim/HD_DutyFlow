359a0c93eef3083be701504ed3d5ac93c31daa5f
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-13 18:18:28 +0900

    Add: PickUpSystem mandatory fields

--- src/pickup/PickUpSystem.java
+++ src/pickup/PickUpSystem.java
@@ -1,5 +1,12 @@
 package pickup;
 
-public class PickUpSystem {
+import java.util.List;
 
+import member.Member;
+
+public class PickUpSystem {
+	private MLPQ pq; 
+	private List<Member> members;
+	private List<Order> orders;
+	private QueueSorter queueSorter;
 }
