270aa2241fc030ed3be079ba972a5d2836b24614
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-05-14 00:05:37 +0900

    Refactor: Builder 패턴 적용 #16

--- src/product/Product.java
+++ src/product/Product.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.util.Locale.Category;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
