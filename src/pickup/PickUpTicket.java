8bc467849b242e56def3bb3a8c710fe073f2bd33
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-13 12:35:07 +0900

    Add: PickUpTicket Constructor

--- src/pickup/PickUpTicket.java
+++ src/pickup/PickUpTicket.java
@@ -2,9 +2,18 @@ package pickup;
 
 import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
 public class PickUpTicket {
 	private Member member;
 	private Airplane airplane;
 	private LocalDateTime ticketIssueTime;
 	private int ticketNum;
+	
+	public PickUpTicket(Member member, Airplane airplane, int ticketNum) {
+		this.member=member;
+		this.airplane=airplane;
+		this.ticketNum=ticketNum;
+	}
 }
