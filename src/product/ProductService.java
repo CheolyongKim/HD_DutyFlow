a2ccf110691f74cbea529c224c14cfe9d389796f
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-05-13 22:58:19 +0900

    Refactor: Exception 공통 부모 처리 #16

--- src/product/ProductService.java
+++ src/product/ProductService.java
@@ -8,20 +8,20 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import common.OracleConnection;
+import exception.ErrorCode;
+import exception.SystemException;
 
 public class ProductService {
 
-	/***
-	 * 
-	 * @return
-	 */
-	public List<productDTO> printAllProducts() {
+
+	public List<productDTO> printAllProducts() throws SystemException {
 
 		List<productDTO> productList = new ArrayList<>();
 
@@ -62,32 +62,32 @@ public class ProductService {
 		        System.out.println("조회 결과 없음");
 		    }
 
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
+		} catch (SQLException e) {
+	        throw new SystemException(ErrorCode.DB_CONNECTION, e); //현재는 DB 를 Main으로 빼지 않았기 때문에 SystemError로 구현
+	    }
 
 		return productList;
 	}
 
-	public static void main(String[] args) {
-
-		ProductService service = new ProductService();
-		List<productDTO> list = service.printAllProducts();
-		
-		for (productDTO dto : list) {
-		    System.out.println("========================================");
-		    System.out.println("상품명      : " + dto.getProductName());
-		    System.out.println("카테고리     : " + dto.getCategory().getCategoryName());
-		    System.out.println("브랜드       : " + dto.getBrandName());
-		    System.out.println("재고        : " + dto.getStockAmount());
-		    System.out.println("용량        : " + dto.getCapacity());
-		    System.out.println("USD 가격    : " + dto.getPriceUsd());
-		    System.out.println("KRW 가격    : " + dto.getPriceKrw());
-		    System.out.println("할인율       : " + dto.getDiscountRate());
-		    System.out.println("임계값       : " + dto.getThresholdValue());
-		    System.out.println("========================================\n");
-		}
-
-	}
+//	public static void main(String[] args) {
+//
+//		ProductService service = new ProductService();
+//		List<productDTO> list = service.printAllProducts();
+//		
+//		for (productDTO dto : list) {
+//		    System.out.println("========================================");
+//		    System.out.println("상품명      : " + dto.getProductName());
+//		    System.out.println("카테고리     : " + dto.getCategory().getCategoryName());
+//		    System.out.println("브랜드       : " + dto.getBrandName());
+//		    System.out.println("재고        : " + dto.getStockAmount());
+//		    System.out.println("용량        : " + dto.getCapacity());
+//		    System.out.println("USD 가격    : " + dto.getPriceUsd());
+//		    System.out.println("KRW 가격    : " + dto.getPriceKrw());
+//		    System.out.println("할인율       : " + dto.getDiscountRate());
+//		    System.out.println("임계값       : " + dto.getThresholdValue());
+//		    System.out.println("========================================\n");
+//		}
+//
+//	}
 	
 }
