545589fa1446d3c47f118d8c7a985c2a1a3b25b2
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-13 16:04:58 +0900

    Feat: MLPQ enqueue

--- src/pickup/MLPQ.java
+++ src/pickup/MLPQ.java
@@ -1,13 +1,24 @@
 package pickup;
 
+import java.time.Duration;
 import java.util.PriorityQueue;
 
 public class MLPQ {
 	private PriorityQueue<PickUpTicket> aq;
-	private PriorityQueue<PickUpTicket> aq;
+	private PriorityQueue<PickUpTicket> bq;
 	
+	private int lastNum;
 	private int promotionThresholdMinutes;
 	private int maxWaitTimeMinutes;
+	
+	public void enqueue(Airplane airplane, Member member) {
+		if (this.size()==0 ||
+				Duration.between(/*Main.curTime*/, airplane.getDepartureAt).getMinutes() < 30) {
+			this.aq.add(new PickUpTicket(member, airplane, ++this.lastNum));
+		}else {
+			this.bq.add(new PickUpTicket(member, airplane, ++this.lastNum));
+		}
+	}
 	
 	public PickUpTicket peek(){
 		if (this.size()==0) {
