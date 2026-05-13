7f5691275eceea9f165f28f2b28cd0ae68d6aba7
0-x-14 <0_x_14@naver.com>
2026-05-13 18:59:40 +0900

    Refactor: ErrorCode 기반 예외 처리 구조 추가

--- src/exception/BusinessException.java
+++ src/exception/BusinessException.java
@@ -5,4 +5,8 @@ public class BusinessException extends DutyFreeException {
     public BusinessException(String errorCode, String message) {
         super(errorCode, message);
     }
+    
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
--- src/exception/QueueException.java
+++ src/exception/QueueException.java
@@ -5,4 +5,8 @@ public class QueueException extends DutyFreeException {
     public QueueException(String errorCode, String message) {
         super(errorCode, message);
     }
+    
+    public QueueException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
