512e6404e34e879233948f1a32ffbb8867fefa5a
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-14 11:20:15 +0900

    Remove: all in Application.main()

--- src/main/Application.java
+++ src/main/Application.java
@@ -10,21 +10,6 @@ import common.OracleConnection;
 public class Application {
 
     public static void main(String[] args) {
-
-    		// DB 연결 테스트입니다. 
-        String sql = "SELECT * FROM Brand";
-
-        try (
-            Connection conn = OracleConnection.getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-        ) {
-            while(rs.next()) {
-                System.out.println( rs.getInt("brandId") + " / " + rs.getString("brandName") + " / " + rs.getInt("managerId"));
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
         
     }
     
