6bda6aff17e7b7c19543438f3b3ae1b673adf0e7
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-14 10:33:46 +0900

    Add: getAllRealPickUp() in PickUpDAO

--- src/pickup/PickUpDAO.java
+++ src/pickup/PickUpDAO.java
@@ -1,5 +1,14 @@
 package pickup;
 
+import java.util.List;
+
+import exception.SystemException;
+import pickup.dto.RealPickUpDTO;
+
 public class PickUpDAO {
 
+	public List<RealPickUpDTO> getAllRealPickUp() throws SystemException{
+		
+	}
+	
 }
