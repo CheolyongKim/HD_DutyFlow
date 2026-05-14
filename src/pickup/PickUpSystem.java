40b0f68f030d2913c9e90a77eafef70f8cfc9425
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-14 09:43:10 +0900

    Chore: overview comment at realPickUp()

--- src/pickup/PickUpSystem.java
+++ src/pickup/PickUpSystem.java
@@ -16,6 +16,14 @@ public class PickUpSystem {
 		 * 파라미터 passportNum, flightResNum: 자신의 순번이 호출되어 인도받으러 온
 		 * 고객이 현실에서 인도장관리자에게 (최종 신원 검증용으로) 제시한 여권번호와 탑승번호
 		 */
+		
+		// 파라미터로 제시된 고객의 정보가 DB에 존재하는지 확인
+		
+		// 존재함
+			// updateOrderState()의 대상 = popQueue()
+			// popQueue()에서 발생 가능한 예외(비어있는 큐에서 pop시도) catch
+		
+		// 존재하지 않음 -> 예외처리
 	}
 	
 	public void appendQueue(String passportNum, int flightResNum) {
