2ce2126206f89f4921ee25b89d2fcdca5c0a6f8f
0-x-14 <0_x_14@naver.com>
2026-05-14 15:14:09 +0900

    Feat: 최신 환율 f 갱신 기능 구현

--- src/exchangeRate/ExchangeRateDAO.java
+++ src/exchangeRate/ExchangeRateDAO.java
@@ -1,5 +1,42 @@
 package exchangeRate;
 
-public class ExchangeRateDAO {
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import exception.ErrorCode;
+import exception.SystemException;
+
+public class ExchangeRateDAO {
+	// 최신 환율 조회
+    public BigDecimal findLatestRate(Connection conn) {
+        String sql = "SELECT exchangeRate FROM ExchangeRate WHERE isLatest = 'Y'";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            if (rs.next()) {
+                return rs.getBigDecimal("exchangeRate");
+            }
+
+            return null;
+
+        } catch (SQLException e) {
+            throw new SystemException(ErrorCode.DB_CONNECTION, e);
+        }
+    }
+    
+    // 기존 최신 환율의 flag값을 N으로 변경
+    public void updateLatestToN(Connection conn) {
+        String sql = "UPDATE ExchangeRate SET isLatest = 'N' WHERE isLatest = 'Y'";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new SystemException(ErrorCode.DB_CONNECTION, e);
+        }
+    }
 }
