06550cc52f6f12e4b7b781c7121c8c8792f4ece0
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-05-14 09:29:17 +0900

    Add: realPickUp() in PickUpSystem

--- src/pickup/PickUpSystem.java
+++ src/pickup/PickUpSystem.java
@@ -3,10 +3,15 @@ package pickup;
 import java.util.List;
 
 import member.Member;
+import order.Order;
 
 public class PickUpSystem {
 	private MLPQ pq; 
 	private List<Member> members;
 	private List<Order> orders;
 	private QueueSorter queueSorter;
+	
+	public void realPickUp(String passportNum, int flightResNum) {
+		
+	}
 }
