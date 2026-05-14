a03bee2037c92e784683e952fe0df4c24b83a232
0-x-14 <0_x_14@naver.com>
2026-05-14 15:09:37 +0900

    Feat: ExchangeRateProvider 싱글톤 구현

--- src/exchangeRate/ExchangeRate.java
+++ src/exchangeRate/ExchangeRate.java
@@ -3,8 +3,13 @@ package exchangeRate;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ExchangeRate {
 	private BigDecimal exchangeRate;
 	private LocalDate exchangeDate;
-	private char isLastest;
+	private char isLatest;
 }
