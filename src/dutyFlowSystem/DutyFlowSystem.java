819eb478f9691d03dfc8df73ab2e841dfbe872a4
0-x-14 <0_x_14@naver.com>
2026-05-14 16:34:04 +0900

    Feat: DutyFlowSystem에 환율 스케줄러 연동

--- src/dutyFlowSystem/DutyFlowSystem.java
+++ src/dutyFlowSystem/DutyFlowSystem.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Queue;
 
 import exchangeRate.ExchangeRate;
+import exchangeRate.ExchangeRateScheduler;
+import exchangeRate.ExchangeRateService;
 import member.Member;
 import order.Order;
 import product.Product;
@@ -15,7 +17,6 @@ public class DutyFlowSystem {
 	private Member member = new Member();
 	private List<Member> members;
 	private Queue<Order> orderQueue;
-	private List<ExchangeRate> exchangeRate;
 	// private List<BrandSystemDao> brandRepository;
 	// private TaxCalculator taxCalculator;
 	
@@ -44,5 +45,21 @@ public class DutyFlowSystem {
 	// 장바구니 선택 상품 제거
 	public void deleteFromCart(List<Product> selectedProducts) { 
 		shoppingCartService.flush(member.getMemberId(), selectedProducts);
+	}
+	
+	// 환율 서비스 및 스케줄러
+	private final ExchangeRateService exchangeRateService = new ExchangeRateService();
+
+	private final ExchangeRateScheduler exchangeRateScheduler = new ExchangeRateScheduler(exchangeRateService);
+
+	// 환율 자동 갱신 스케줄러 시작
+	public void startExchangeRateScheduler() {
+		exchangeRateScheduler.start();
+	}
+
+	// 환율 자동 갱신 스케줄러 종료
+	// 스케줄러 lifecycle을 명시적으로 관리하기 위해 추가
+	public void stopExchangeRateScheduler() {
+		exchangeRateScheduler.stop();
 	}
 }
