67c9e4156442d6f97ec81fc1c8458a13f8f81b3c
0-x-14 <0_x_14@naver.com>
2026-05-15 09:14:27 +0900

    Feat: 결제 도메인 모델 및 DTO 추가

--- src/payment/Payment.java
+++ src/payment/Payment.java
@@ -1,5 +1,21 @@
 package payment;
 
-public class Payment {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class Payment {
+    private final int paymentId;
+    private final int orderId;
+    private final String paymentMethod;
+    private final PaymentStatus paymentStatus;
+    private final BigDecimal requestedAmount;
+    private final String cardNumberMask;
+    private final LocalDateTime requestedAt;
+    private final LocalDateTime processedAt;
+    private final String failReason;
 }
